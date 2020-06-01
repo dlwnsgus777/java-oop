3985b7e0752d2e281a61ca8eb91bbf011e959805
Lee, JunHyun <dlwnsgus777@naver.com>
2020-06-01 16:46:48 +0900

    feat: 계산 로직을 어떻게할까...

--- src/main/java/bowling/domain/Pins.java
+++ src/main/java/bowling/domain/Pins.java
@@ -3,7 +3,6 @@ package bowling.domain;
 public class Pins {
 
     private static final int MAX_BOWLING_PIN = 10;
-    private static final int MIN_BOWLING_PIN = 0;
 
     private int bowlingPins;
 
@@ -14,12 +13,10 @@ public class Pins {
     public int rollingBall() {
         int randomNumber = (int)(Math.random() * bowlingPins) + 1;
         hitBowlingPin(randomNumber);
-        System.out.println("볼링핀 " + bowlingPins + " 개 남음");
-        return MAX_BOWLING_PIN - bowlingPins;
+        return randomNumber;
     }
 
     public void resetPin() {
-        System.out.println("reset call");
         this.bowlingPins = MAX_BOWLING_PIN;
     }
 
