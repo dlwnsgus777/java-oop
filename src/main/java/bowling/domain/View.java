419ed787bfde4c370fe52f9a6af1e6d423628ee5
Lee, JunHyun <dlwnsgus777@naver.com>
2020-07-01 17:01:29 +0900

    feat: 후다닥 끝내자

--- src/main/java/bowling/domain/View.java
+++ src/main/java/bowling/domain/View.java
@@ -25,22 +25,22 @@ public class View {
 //        System.out.print(state + " == " + totalScore);
     }
 
-    private void setScore(int[] scores, String state) {
-        for(int i = 0; i < scores.length; i++) {
-           String convertScore =  convertBowlingScore(scores[i], state);
-           score += "" + convertScore + "  ";
-           score.trim();
-        }
-    }
-
     private void setRound(int frameNumber) {
-        String thisRound = frameNumber + " Frame  || ";
+        String thisRound = frameNumber + " Frame\t||\t";
         round += thisRound;
         round.trim();
     }
 
+    private void setScore(int[] scores, String state) {
+        for(int i = 0; i < scores.length; i++) {
+            String convertScore =  convertBowlingScore(scores[i], state);
+            score += "" + convertScore + "  ";
+            //score.trim();
+        }
+    }
+
     private String convertBowlingScore(int hitPin, String state) {
-        String bowlingSocre = " " + hitPin + "";
+        String bowlingSocre = hitPin + "\t";
 
         if (hitPin == 0) {
             bowlingSocre = "-";
